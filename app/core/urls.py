e115d1f4c699900c091a8baca3e73f6d2e00a73b
Jabba <87302129+jabb4@users.noreply.github.com>
2024-10-22 13:05:35 +0200

    changed root url to homepage

--- app/core/urls.py
+++ app/core/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.example, name="example"),
+    path("", views.homepage_view, name="homepage"),
     path("login/", views.login_view, name="login"),
     path("logout/", views.logout_view, name="logout"),
     path("register/", views.register_view, name="register"),
