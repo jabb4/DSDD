753eafa922613740f276c377d4b98fc5bc1fdc1d
Jabba <87302129+jabb4@users.noreply.github.com>
2024-10-22 13:05:21 +0200

    added example model to admin panel

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import User, APIKey
+from .models import User, APIKey, ExampleModel
 
 # Register your models here.
 
 admin.site.register(User)
 admin.site.register(APIKey)
+admin.site.register(ExampleModel)
